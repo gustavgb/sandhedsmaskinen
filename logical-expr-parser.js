d1b38e50f57bce5d828efc8ffd7da007358282b5
Gustav Burchardt <gustav@gbur.dk>
2022-02-16 20:16:26 +0100

    Implement cli version

--- logical-expr-parser.js
+++ logical-expr-parser.js
@@ -1,6 +1,21 @@
-const validCharacters = [
-  '!', '&', '|', '->', '<->', '=', '(', ')',
+const util = require('util')
+
+function print (obj) {
+  console.log(util.inspect(obj, false, null, true))
+}
+
+const operators = [
+  '!', '&', '|', '->', '<->'
+]
+
+const letters = [
   'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Z', 'Y'
+]
+
+const validCharacters = [
+  '=', '(', ')', ',',
+  ...operators,
+  ...letters
 ]
 
 function parseCharacters (expr) {
@@ -41,51 +56,12 @@ function parseCharacters (expr) {
 // }
 
 /**
- * Splits expression
- * @param {Array} chars Chars
- * @returns Array
- */
-function splitExpression (expr) {
-  const chars = parseCharacters(expr)
-
-  const stack = []
-  const indexStack = []
-
-  const vufs = []
-  for (let i = 0; i < chars.length; i++) {
-    if (chars[i] === '(') {
-      stack.push('(')
-      indexStack.push(i + 1)
-    } else if (chars[i] === ')') {
-      if (stack.length !== 0) {
-        const stackTop = stack.pop()
-
-        if (stackTop === '(') {
-          const startIndex = indexStack.pop()
-
-          vufs.push(chars.slice(startIndex, i))
-        } else {
-          throw new Error('Parantheses not matching')
-        }
-      } else {
-        throw new Error('Parantheses not matching')
-      }
-    }
-  }
-
-  if (stack.length !== 0) {
-    throw new Error('Parantheses not matching')
-  }
-
-  return vufs
-}
-
-/**
  * Returns multidimensional array
- * @param {Array} chars Chars
+ * @param {Array} expression Expression
  */
-function deepenExpression (chars) {
-  const result = []
+function makeTree (expression) {
+  const chars = parseCharacters(expression)
+  const result = [null]
 
   const stack = [result]
 
@@ -95,6 +71,8 @@ function deepenExpression (chars) {
       stack.push(stack[stack.length - 1][stack[stack.length - 1].length - 1])
     } else if (chars[i] === ')') {
       stack.pop()
+    } else if (letters.indexOf(chars[i]) > -1) {
+      stack[stack.length - 1].push([null, chars[i]])
     } else {
       stack[stack.length - 1].push(chars[i])
     }
@@ -103,93 +81,240 @@ function deepenExpression (chars) {
   return result
 }
 
-function determineExpressionHelper (options, tree) {
+/**
+ * Determine tree
+ * @param {Object} options Options
+ * @param {Array} tree Tree
+ * @returns Array
+ */
+function determineTree (options, tree) {
+  if (tree.length === 2 && letters.indexOf(tree[1]) > -1) {
+    tree[0] = !!options[tree[1]]
+  } else if (tree.length === 2) {
+    tree[0] = !!determineTree(options, tree[1])[0]
+  } else if (tree[2] === '->') {
+    const a = determineTree(options, tree[1])[0]
+    const b = determineTree(options, tree[3])[0]
 
-}
+    tree[0] = !!(
+      (a && b) ||
+      (!a && !b) ||
+      (!a && b)
+    )
+  } else if (tree[2] === '|') {
+    const a = determineTree(options, tree[1])[0]
+    const b = determineTree(options, tree[3])[0]
 
-function determineExpression (options, tree) {
-  for (let i = 1; i < tree.length; i++) {
-    if (Array.isArray(tree[i])) {
-      tree[i] = determineExpression(options, tree[i])
-    }
-  }
-
-  if (tree[1] === '!') {
-    tree[0] = determineExpression(options, tree[i])
+    tree[0] = !!(a || b)
   } else if (tree[2] === '&') {
+    const a = determineTree(options, tree[1])[0]
+    const b = determineTree(options, tree[3])[0]
 
+    tree[0] = !!(a && b)
+  } else if (tree[2] === '<->') {
+    const a = determineTree(options, tree[1])[0]
+    const b = determineTree(options, tree[3])[0]
+
+    tree[0] = !!(
+      (a && b) || (!a && !b)
+    )
+  } else if (tree[1] === '!') {
+    const a = determineTree(options, tree[2])[0]
+
+    tree[0] = !a
   }
 
   return tree
 }
 
 /**
- *
- * @param {String} vuf
- * @param {Number} i
- * @param {String} operator
- * @param {Array} parts
+ * Determine expression
+ * @param {Object} options Options
+ * @param {String} expression Expression
+ * @returns Array
  */
-function parseVufOperatorHelper (vuf, i, parts) {
-  return function curried (operator) {
-    if (vuf.substr(i, operator.length) === operator) {
-      parts.push(vuf.substring(0, i))
-      parts.push(vuf.substring(i, operator.length))
-      parts.push(vuf.substr(operator.length + 1))
-      return true
+function determineExpression (options, expression) {
+  return determineTree(options, makeTree(expression))
+}
+
+/**
+ * Sentence
+ * @param {String} sentence Sentence
+ * @returns Array<Array>
+ */
+function splitSentence (sentence) {
+  return sentence.split('=').map(side => side.split(',').map(a => a.trim()))
+}
+
+/**
+ * Determine Sentence
+ * @param {Object} options Options
+ * @param {String} sentence Sentence
+ * @returns Array
+ */
+function determineSentence (options, sentence) {
+  const sentenceParts = splitSentence(sentence)
+    .map(expressions => expressions.map(expr => determineExpression(options, expr)))
+
+  if (sentenceParts.length !== 2) {
+    sentenceParts.unshift(null)
+    return sentenceParts
+  }
+
+  const truePremises = sentenceParts[0].every(part => part[0])
+  const falseConclusion = sentenceParts[1].some(part => !part[0])
+
+  if (truePremises && falseConclusion) {
+    sentenceParts.unshift(false)
+  } else {
+    sentenceParts.unshift(true)
+  }
+
+  return sentenceParts
+}
+
+function determineTable (sentence) {
+  const variations = []
+  const usedLetters = []
+
+  for (let i = 0; i < sentence.length; i++) {
+    if (letters.indexOf(sentence[i]) > -1 && usedLetters.indexOf(sentence[i]) === -1) {
+      usedLetters.push(sentence[i])
     }
-    return false
+  }
+
+  const totalVariations = Math.pow(2, usedLetters.length)
+  for (let i = 0; i < totalVariations; i++) {
+    const variation = {}
+    for (let j = 0; j < usedLetters.length; j++) {
+      variation[usedLetters[j]] = (Math.floor((i / (totalVariations / Math.pow(2, j + 1))) % 2) === 0)
+    }
+    variations.push(variation)
+  }
+
+  return variations.map(variation => [
+    variation,
+    determineSentence(variation, sentence)
+  ])
+}
+
+/**
+ *
+ * @param {Array} tree Tree
+ * @returns String
+ */
+function printExpressionTreeHeader (tree, isRoot) {
+  if (Array.isArray(tree)) {
+    if (tree.length > 2) {
+      if (isRoot === true) {
+        return tree.slice(1).map(printExpressionTreeHeader).join(' ')
+      }
+      return `(${tree.slice(1).map(printExpressionTreeHeader).join(' ')})`
+    } else {
+      return printExpressionTreeHeader(tree[1])
+    }
+  } else {
+    return tree
   }
 }
 
-function parseVuf (vuf) {
-  const stack = []
-  const parts = []
-
-  for (let i = 0; i < vuf.length; i++) {
-    if (stack.length === 0) {
-      ['!', '&', '|', '->', '<->'].find(parseVufOperatorHelper(vuf, i, parts))
-    }
-
-    if (vuf[i] === '(') {
-      stack.push('(')
-    } else if (vuf[i] === ')') {
-      stack.pop()
-    }
+function printBoolean (bool) {
+  if (bool) {
+    return 's'
   }
-
-  return parts
+  return 'f'
 }
 
-function determineVufStack (values, expr) {
-  const cache = {
-    ...values
+function printValid (bool) {
+  if (bool) {
+    return 'valid'
+  } else if (bool === false) {
+    return 'invalid'
   }
-
-  const stack = splitExpression(expr)
-
-  stack.forEach(vuf => {
-    console.log(parseVuf(vuf))
-  })
+  return ''
 }
 
-// console.log(determineVufStack({ P: true, Q: true, S: true }, '(((P&Q)->(R|Q))|(!(P<->(S->Q))))'))
+/**
+ *
+ * @param {Array} tree Tree
+ * @returns String
+ */
+function printExpressionTree (tree, isRoot) {
+  if (Array.isArray(tree)) {
+    if (tree.length === 4) {
+      return `${printExpressionTree(tree[1])} ${printBoolean(tree[0])} ${printExpressionTree(tree[3])}`
+    } else if (tree.length === 3) {
+      return `${printBoolean(tree[0])} ${printExpressionTree(tree[2])}`
+    } else if (tree.length === 2) {
+      return printBoolean(tree[0])
+    }
+  }
+}
 
-function main ({ expression }) {
-  console.log(
-    determineExpression(
-      options,
-      deepenExpression(
-        parseCharacters(
-          expression
-        )
-      )
-    )
+function printOptions (letters, options) {
+  return letters.map(letter => printBoolean(options[letter])).join(' ')
+}
+
+function printTable (sentence) {
+  const determinations = determineTable(sentence)
+  const letters = Object.keys(determinations[0][0])
+
+  const sentenceHeader = determinations[0][1].slice(1)
+    .map(side => side.map(tree => printExpressionTreeHeader(tree, true)).join(', '))
+    .join(' = ')
+
+  const spaces = sentenceHeader.split(' ').reduce(
+    (acc, component, index, arr) => {
+      if (index === arr.length - 1) {
+        return acc
+      }
+
+      if (component === '=') {
+        acc[acc.length - 1] += 2
+        return acc
+      }
+
+      for (let i = 0; i < component.length; i++) {
+        if (component[i] !== '(') {
+          acc.push(component.length - 1 - i)
+          break
+        } else {
+          acc[acc.length - 1] += 1
+        }
+      }
+      return acc
+    },
+    [0]
   )
+
+  const lines = [
+    `[ ${letters.join(' ')} ]   ${sentenceHeader}`,
+    ...determinations.map(determination => {
+      const lineValues = determination[1].slice(1)
+        .map(side => side.map(tree => printExpressionTree(tree)).join(' '))
+        .join(' ')
+        .split(' ')
+        .map((value, index) => [...new Array(spaces[index])].fill(' ').join('') + value)
+        .join(' ')
+
+      return `[ ${printOptions(letters, determination[0])} ]   ${lineValues}    ${printValid(determination[1][0])}`
+    })
+  ]
+
+  lines.forEach(line => console.log(line))
 }
 
-main({
-  options: { P: true, Q: true, S: true },
-  expression: '(((P&Q)->(R|Q))|(!(P<->(S->Q))))'
+function main (sentence) {
+  const result = printTable(sentence)
+
+  console.log(util.inspect(result, false, null, true))
+}
+
+main(
+  // '(((P&Q)->(R|Q))|(!(P<->(S->Q))))',
+  // '(((((Q)->(R))|((Q)->(!(R))))->((P)|(Q)))<->(P))'
   // expression: '((P&Q)->(R|Q))'
-})
+  // expression: '((P&Q)->(P|Q))'
+  // '(P->Q),(Q)=(P)'
+  '(((P)->(Q))|((!(R))->(Q))), (!((R)&(P))) |= ((Q)|(P))'
+)
